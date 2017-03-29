567b505e0a598ce1d5775ec00940b0451b8c574c
Solemnity <gijswow@gmail.com>
2017-03-29 16:01:46 +0200

    Initial commit

---
+++ views/index.php
@@ -0,0 +1,25 @@
+<?php
+// ------------------------------------------------------------------------
+// Maak het onderstaande script werkend:
+// Vul de code aan en zorg dat de include-bestanden in de juiste map staan
+// ------------------------------------------------------------------------
+
+// hier komen de instellingen van deze web app
+include 'includes/config';
+
+// hier komt de head sectie <head> etc.
+include 'views/head';
+
+// hier komt de header sectie etc.
+include 'views/header';
+
+// hier komt de navigatie
+include 'views/menu';
+
+// hier komt de content van de home page
+include 'views/flatindex';
+
+// hier komt de footer
+include 'views/footer'
+
+?>
